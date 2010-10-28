9426397c8d8a25255209d19d6095fa2468f7792b
Asaf <asaf.tvn@gmail.com>
2010-10-28 09:18:29 +0200

    started LoginBox project

---
+++ advertiser/advertiser/Static/js/LoginBox.js
@@ -0,0 +1,21 @@
+dojo.provide("tvw.LoginBox");
+
+dojo.require("dijit._Widget");
+dojo.require("dijit._Templated");
+dojo.require("dijit._CssStateMixin");
+
+dojo.declare(
+	"tvw.LoginBox",
+	[dijit._Widget, dijit._Templated],
+	{
+		// summary:
+		//		A simple GUI for secure login over ajax
+	        //              (passes only hashed password to the server)
+	    
+	    baseClass: "tvwLoginBox",
+
+	    templateString: dojo.cache("dijit", "templates/LoginBox.html"),
+
+	}
+);
+	    
