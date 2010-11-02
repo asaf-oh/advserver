263ca19c5d65518d50efc6a4583a766ccb172bf9
Asaf <asaf.tvn@gmail.com>
2010-11-03 01:22:59 +0200

    removed aspnet/dojo

--- advertiser/NHibernate-3.0.0-Beta1/src/NHibernate.Test/Linq/Entities/AnotherEntity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NHibernate.Test.Linq.Entities
-{
-    public class AnotherEntity
-    {
-        public virtual int Id { get; set; }
-        public virtual string Output { get; set; }
-    }
-}
--- advertiser/advertiser/UTest/Mocks/ISession.cs
+++
@@ -1,8 +0,0 @@
-using System;
-namespace advertiser
-{
-	public interface ISession
-	{
-	}
-}
-
