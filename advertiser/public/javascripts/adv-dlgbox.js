6db9313a0f305c9ecfc66115cbc10ef05e950b65
Asaf <asaf.tvn@gmail.com>
2010-11-04 00:58:48 +0200

    have dlg box

--- advertiser/public/javascripts/adv-dlgbox.js
+++ advertiser/public/javascripts/adv-dlgbox.js
@@ -15,8 +15,51 @@ function get_renderer(element)
 }
 
 
-advBullet = Class.create();
-advBullet.prototype = {
+
+advFrame = Class.create( {
+    /*
+      Function: initialize
+      Constructor. 
+      
+      Returns:
+      A new advFrame
+    */
+    initialize: function(renderer) {
+	this.renderer = renderer;
+	this.size = renderer.getSize();
+
+	this.rect = new Graphic.Rectangle(renderer);
+	
+	return this;
+    },
+
+    /*
+      Function: render      
+      
+      adds frame to renderer
+      
+    */  
+    render: function()
+    {
+	const stroke_w = 5;
+	const padding = 20;
+    
+	var rect = this.rect;
+	var width = this.size.width - stroke_w*2 - padding*2;
+	var height = this.size.height - stroke_w*2 - padding*2;
+    
+	rect.setStroke({r: 255, g: 255, b: 255, w:stroke_w, a:0});
+	rect.setBounds(stroke_w + padding, stroke_w + padding, width, height);
+	rect.setFill({r: 44, g: 122, b: 199, a: 0});
+	rect.setRoundCorner(20, 25);
+	rect.setID("login")
+
+	this.renderer.add(rect);
+    }
+}); // advFrame
+
+
+advBullet = Class.create( {
     /*
       Function: initialize
       Constructor. 
@@ -45,73 +88,43 @@ advBullet.prototype = {
 
 	// 
 	var inner_color = {r: 240, g: 161, b: 61, a:0}
-	var pos = {x: 20, y: 20};
+	var pos = {x: 75, y: 4};
 	var inn_size = {width : this.size.width/1.5, height: this.size.height/1.5};
 	var inn_pos = { x: pos.x + (this.size.width - inn_size.width)/2 ,
 			y: pos.y + (this.size.height - inn_size.height)/2 };
 
 	this.inner.setBounds(inn_pos.x, inn_pos.y, inn_size.width, inn_size.height);
-
 	this.inner.setFill(inner_color);
 
 	this.outer.setBounds(pos.x, pos.y, this.size.width, this.size.height);
+	this.outer.setFill({r: 44, g:122, b:199});
 	this.outer.setStroke({r: 255, g: 255, b: 255, w:5, a:0});
 
-	this.renderer.add(this.inner);
 	this.renderer.add(this.outer);
+	this.renderer.add(this.inner);
     }
 
-}
+}); // advBullet
 
-/*
-advDlgButton = Class.create();
-advDlgButton.prototype = {
-    initialize: function(renderer) {
-	this.renderer = renderer;
-	this.size = {width:50, height:50};
+advDlgBox =  Class.create( {
+    /* Constructor
+     */
+    initialize : function(element, options)    {
+	console.log("advDlgBox : Enter - " + element);
+	this.element = element;
+	this.renderer = get_renderer(element);
 
-	this.outer = new Graphic.Circle(renderer);
-	this.inner = new Graphic.Circle(renderer);
-	
-	return this;
+	this.frame = new advFrame(this.renderer);
+	this.title = element.firstDescendant();	
+	this.titleBullet = new advBullet(this.renderer);
+
+	this.position_title();
+	this.frame.render();
+	this.titleBullet.render();
     },
 
-    render: function()
-    {
-	// var dlg_size = renderer.getSize();
+    position_title : function() {
+	this.title.setStyle({'top':'-10px', 'left':'127px', 'padding':'0px 5px 0px 10px'});
+    }    
 
-	// 
-	var inner_color = {r: 240, g: 161, b: 61, a:0}
-	this.inner.setBounds(68.5,68.5, this.size.width/1.5, this.size.height/1.5);
-	//	this.inner.setStroke();
-	this.inner.setFill(inner_color);
-
-	this.outer.setBounds(60,60, this.size.width, this.size.height);
-	this.outer.setStroke({r: 255, g: 255, b: 255, w:5, a:0});
-
-	this.renderer.add(this.inner);
-	this.renderer.add(this.outer);
-    }
-}
-*/
-
-function adv_dialog(element, options)
-{
-    const stroke_w = 8;
-    const renderer = get_renderer(element);
-    
-    var rect = new Graphic.Rectangle(renderer); 
-    var width = parseInt($(element).getStyle("width"))-stroke_w*2;
-    var height = parseInt($(element).getStyle("height"))-stroke_w*2;
-    
-    rect.setStroke({r: 255, g: 255, b: 255, w:stroke_w, a:0});
-    rect.setBounds(stroke_w, stroke_w, width, height);
-    rect.setFill({r: 44, g: 122, b: 199, a: 0});
-    rect.setRoundCorner(20, 25);
-    rect.setID("login")
-
-    renderer.add(rect);
-
-    title_bullet = new advBullet(renderer);
-    title_bullet.render();
-}
+}); // advDlgBox
